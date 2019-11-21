adc27889c767a49567f23e61ab5a02b1662db904
Molly Stambaugh <molly.stambaugh@gmail.com>
2019-11-20 23:29:29 -0800

    add a few notes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,7 +5,8 @@ import Error404 from './Error404';
 import Header from './Header';
 import { Switch, Route } from 'react-router-dom';
 
-// We need the boolean data value is pet alive or dead to be application state, because it dictates the game continuing or ending (if it ends we might route to the new pet path and allow for game start over. All other data--hunger level, love level, etc, can be application state and call on relevant child components. 
+// We need the boolean data value is pet alive or dead to be application state, because it dictates the game continuing or ending. All other data--hunger level, love level, etc, can be application state and call on  child components to perpetuate the game flow with on clicks and event listeners and communicating info to PetStatus if the pet runs out of love or food, changing the bool value stored in Petstatus from true to false, giving that info to App, and ending the game state, switching to the new game route.
+ 
 class App extends React.Component {
 
   constructor(props) {
